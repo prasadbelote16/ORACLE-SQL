50d03ac39d731649596d3af97428c78be60445d2
Prasad Belote <95467373+prasadbelote16@users.noreply.github.com>
2025-10-21 19:45:13 +0530

    Refactor and update Oracle string functions documentation

--- STRING FUNCTIONS.sql
+++ STRING FUNCTIONS.sql
@@ -63,4 +63,67 @@ SELECT RPAD('ABC',5,'*') FROM DUAL; --ABC**
 SELECT RPAD('ABC',2,'*') FROM DUAL; --AB
 
 --16)TRANSLATE :-Replace all occurrences of characters by other characters in a string.
-SELECT TRANSLATE ()
+SELECT TRANSLATE ('[asdfh()]','[()]','{\/}') from dual; ---{asdfh\/}
+
+--17)REPLACE :-Replace all occurrences of a substring by another substring in a string.
+SELECT REPLACE ('PRASAD BELOTE','PRASAD BELOTE','PRASAD UTTAM BELOTE') FROM DUAL; --PRASAD UTTAM BELOTE
+
+--18)SUBSTR :-Extract a substring from a string.
+SELECT SUBSTR('FQJPB4551H',6,4) FROM DUAL; --4551 WHERE 6 START POSITION AND 4 IS OCCURENCES REQUIRED 
+SELECT SUBSTR('FQJPB4551H',3) FROM DUAL; --JPB4551H RETURN ALL VALUES AFTER 3RD INDEX
+SELECT SUBSTR('FQJPB4551H',-5,3) FROM DUAL; --455  -5 EXTRACT LAST 5 CHAR AS IT IS NEGATIVE AND THEN GIVE FIRST 3 CHAR FROM INDEX
+
+--19)INSTR :-Search for a substring and return the location of the substring in a string
+SELECT INSTR( 'PRASAD','A') FROM DUAL; --3  --FIRST OCCURENCE
+SELECT INSTR( 'PRASAD','A',1,2) FROM DUAL;--5 --SECOND OCCURENCE
+SELECT INSTR( 'PRASAD','M') FROM DUAL;  --0
+
+--20)
+--============================REGULAR EXPRESSIONS
+--'\d+'  == \d means 'digit'. + means, '1 or more times'. So \d+ means one or more digit.
+--1}REGEXP_COUNT := Return the number of times a pattern occurs in a string.
+SELECT REGEXP_COUNT('PRASAD123123123','123') FROM DUAL; --3 
+
+--2}REGEXP_INSTR :=Return the position of a pattern in a string.
+SELECT 
+    REGEXP_INSTR(
+        'If you have any question please call 123-456-7890 or (123)-456-7891',
+        '(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})') First_Phone_No
+FROM
+    dual;
+
+--3}REGEXP_LIKE :=Match a string based on a regular expression pattern.
+SELECT
+  first_name
+FROM
+  employees
+WHERE
+  REGEXP_LIKE( first_name, 'c' )
+ORDER BY first_name;
+
+--4}REGEXP_REPLACE :=Replace substring in a string by a new substring using a regular expression.
+SELECT 
+    REGEXP_REPLACE('Th♥is∞ is a dem☻o of REGEXP_♫REPLACE function','[^a-z_A-Z ]') 
+FROM 
+    dual;
+    
+--5}REGEXP_SUBSTR :=Extract substrings from a string using a pattern of a regular expression.
+SELECT regexp_substr ( 'Oracle 23ai', '\d+' ) FROM DUAL;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
